9c8c7f368ed6aab4a7d28c236060c98b99506eb4
YUHAO FEI <ffhhfyh@gmail.com>
2017-06-29 11:12:10 -0400

    Edit 47
    Still lack complexity information!

--- week4/Thur/47_PermutationsII.java
+++ week4/Thur/47_PermutationsII.java
@@ -10,8 +10,15 @@ For example,
 ]
 */
 
+/* 
+	This question's point is how to know distinguish horizonal and vertical position!
+	Complexity:
+	Time ? 
+	Space  ?
+*/
 
-public List<List<Integer>> permuttationII (int[] nums) {
+
+public List<List<Integer>> permutationII (int[] nums) {
 	List<List<Integer>> res = new ArrayList<List<Integer>>();
 	if(nums == null || nums.length == 0) {
 		return res;
@@ -23,14 +30,17 @@ public List<List<Integer>> permuttationII (int[] nums) {
 }
 
 private void helper(List<List<Integer>> res, List<Integer> path, int[] nums, boolean[] visited) {
-	// base
-	if (path.size() == nums.length) {
+	// base: the length is reached, add a new element and return.
+	if (path.size() == nums.length) {   
 		res.add(new ArrayList<Integer>(path));
 		return;
 	}
 
 	for (int i = 0; i < nums.length; i++) {
-		if (visited[i] || i != 0 && nums[i] == nums[i-1] && !visited[i-1]) {
+		// No vertical duplicates; 当nums[i] == nums[i-1]，但是访问标签却显示nums[i-1]当前没有被访问过
+		// 这恰恰说明这两个数字现在是在不同的path上, 并且前一条path已经遍历过。如果它们在相同的path之上，那么
+		// 基于纵向去重的原理，前一个必然已经被访问过了。
+		if (visited[i] || i != 0 && nums[i] == nums[i-1] && !visited[i-1]) { // If "!visited[i-1]" is changed to "visited[i-1]", it will still work.
 			continue;
 		}
 
