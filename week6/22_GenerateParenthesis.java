39d5ef59179b1d37a5fc070a37bb633fe17036dd
YUHAO FEI <ffhhfyh@gmail.com>
2017-07-17 23:32:33 -0400

    commit

---
+++ week6/22_GenerateParenthesis.java
@@ -0,0 +1,38 @@
+// Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+// For example, given n = 3, a solution set is:
+
+// [
+//   "((()))",
+//   "(()())",
+//   "(())()",
+//   "()(())",
+//   "()()()"
+// ]
+
+public List<String> generateParenthesis(int n) {
+	List<String> res = new ArrayList<>();
+	helper(res, new String(), n, 0);
+	return res;
+}
+// counter is the number of "(" which hasn't been paired?
+private void helper(List<String> res, String str, int n, int counter) { // What is this counter?
+// End case: all "(" have been paired, and there are n pairs.
+	if(counter == 0 && str.length() == 2 * n) {
+		return;
+	}
+// corner case: all invalid situations
+	if(counter < 0 || counter > n || str.length() > 2 * n) {
+		return;
+	}
+// There is at least one "(" which has not been paired by a ")"
+	if(counter > 0 && counter < n) {
+		helper(res, str + "(", n, counter + 1);
+		helper(res, str + ")", n, counter - 1);
+	} else if(counter == 0) { // All "(" have been paired
+		helper(res, str + "(", n , counter + 1);
+	} else if {
+		helper(res, str + ")", n, counter - 1);
+	}
+	return;
+}
