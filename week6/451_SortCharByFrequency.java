6eb21367188f626584a10ceef49f488a8d177b13
YUHAO FEI <ffhhfyh@gmail.com>
2017-07-23 22:50:25 -0400

    commit

---
+++ week6/451_SortCharByFrequency.java
@@ -0,0 +1,58 @@
+// Given a string, sort it in decreasing order based on the frequency of characters.
+
+// Example 1:
+
+// Input:
+// "tree"
+
+// Output:
+// "eert"
+
+// Explanation:
+// 'e' appears twice while 'r' and 't' both appear once.
+// So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+// Example 2:
+
+// Input:
+// "cccaaa"
+
+// Output:
+// "cccaaa"
+
+// Explanation:
+// Both 'c' and 'a' appear three times, so "aaaccc" is also a valid answer.
+// Note that "cacaca" is incorrect, as the same characters must be together.
+// Example 3:
+
+// Input:
+// "Aabb"
+
+// Output:
+// "bbAa"
+
+// Explanation:
+// "bbaA" is also a valid answer, but "Aabb" is incorrect.
+// Note that 'A' and 'a' are treated as two different characters.
+
+public class Solution {
+    public String frequencySort(String s) {
+        HashMap<Character, Integer> charFreqMap = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            charFreqMap.put(c, charFreqMap.getOrDefault(c, 0) + 1);
+        }
+        ArrayList<Map.Entry<Character, Integer>> list = new ArrayList<>(charFreqMap.entrySet());
+        list.sort(new Comparator<Map.Entry<Character, Integer>>(){
+            public int compare(Map.Entry<Character, Integer> o1, Map.Entry<Character, Integer> o2) {
+                return o2.getValue().compareTo(o1.getValue());
+            }
+        });
+        StringBuffer sb = new StringBuffer();
+        for (Map.Entry<Character, Integer> e : list) {
+            for (int i = 0; i < e.getValue(); i++) {
+                sb.append(e.getKey());
+            }
+        }
+        return sb.toString();
+    }
+}
